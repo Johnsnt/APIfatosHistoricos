d34485ef71007e8b0d05675ed4d94a6012004ff1
Johnny Santos <149924300+Johnsnt@users.noreply.github.com>
2024-03-19 15:44:05 -0300

    projeto finalizado, falta só dar umas revisão

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const app = express();
 
-const buscarFatos = require('./service/buscarAno');
+const { buscarFatosPorAno, validarAno } = require('./service/buscarAno.js');
 
 app.get('/', (req, res) => {
+
+
     //Requisição
     let fatoAno = req.query.ano;
-    let fato = buscarFatos.buscarFatosPorAno(fatoAno);
-
-    res.json(fato);
+    if (validarAno(fatoAno)) {
+        var fato = buscarFatosPorAno(fatoAno);
+        res.json(fato);
+    } else {
+        res.status(400).json({ erro: 'Parâmetro inválido' });
+    }
 });
 
 app.listen(1000, () => {
     let date = new Date();
     let hour = date.getHours();
     let minutes = date.getMinutes();
-    console.log(`Servidor iniciado na date: ${date.toLocaleDateString('pt-BR')} às ${hour}:${minutes}`);
+    console.log(`Servidor iniciado na porta 1000 em: ${date.toLocaleDateString('pt-BR')} às ${hour}:${minutes}`);
 });
--- service/buscarAno.js
+++ service/buscarAno.js
@@ -1,9 +1,27 @@
-let data = require('../data/fatos');
+const data = require('../data/fatos');
+
+function validarAno(anoEscolhido) {
+    if (isNaN(anoEscolhido)){
+        return false
+    }
+    else {
+        if (anoEscolhido >= 1920 && anoEscolhido <= 2020){
+            return true
+        } else {
+            return false
+        }
+    }
+}
 
 function buscarFatosPorAno(anoEscolhido) {
-    
     const fatoEscohido = data.fatos.filter((data) => data.ano == anoEscolhido);
     return fatoEscohido[0];
 }
 
-exports.buscarFatosPorAno = buscarFatosPorAno;
+
+module.exports = {
+    buscarFatosPorAno: buscarFatosPorAno,
+    validarAno: validarAno
+}
+
+
